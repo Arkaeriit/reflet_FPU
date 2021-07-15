5e9c5566d5df9fa5bbb22df2c89f623bf08bc81d
Arkaeriit <maxbouillot@gmail.com>
2021-07-16 00:09:58 +0200

    Fixed float to int
    When the nuber was big enought
    to induce some accuracy lost,
    the converted integer was absurd.
    This is now fixed.

--- floating_points_opperations/reflet_float_to_int.v
+++ floating_points_opperations/reflet_float_to_int.v
@@ -20,14 +20,16 @@ module reflet_float_to_int #(
     //Decoding mantissa
     wire [mantissa_size(float_size)-1:0] mantissa = float_in[mantissa_size(float_size)-1:0];
     wire [mantissa_size(float_size):0] value = {1'b1, mantissa};
-    wire [int_size-2:0] ret_abs = value >> (mantissa_size(float_size) - exponent);
+    wire [exponent_size(float_size):0] shift = (mantissa_size(float_size) - exponent);
+    wire [int_size-2:0] ret_abs = ( shift[exponent_size(float_size)-1] //Depending on the value of the exponent, the dirrection of the shift is not th same
+                                        ? value << (exponent - mantissa_size(float_size))
+                                        : value >> (mantissa_size(float_size) - exponent) );
 
     //Solving edge cases
     wire [int_size-2:0] ret_spcs = ( float_in[float_size-1:0] == 0 ? 0 : // 0
                                      ( &exponent ? 1 : //exponent = -1
                                        ( exponent[exponent_size(float_size)-1] ? 0 : //small exponent 
                                          ( exponent >= int_size ? ~0 : //Big number
-                                        
                                            ( ret_abs ))))); //normal case
 
     //Decoding sign
