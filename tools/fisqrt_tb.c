3317200575a04449ecb746024b94b9c99f945fa6
Arkaeriit <maxbouillot@gmail.com>
2021-07-17 19:41:05 +0200

    Tiddy up fisqrt_tb.c

--- tools/fisqrt_tb.c
+++ tools/fisqrt_tb.c
@@ -1,5 +1,18 @@
+/*----------------------\
+|This programs does     |
+|the same computation as|
+|the fisqrt simulation  |
+|to compare the results.|
+\----------------------*/
+
 #include <stdio.h>
 #include <inttypes.h>
+
+#if 0 //Toogle for a more verbos output
+#define trace(X...) printf(X)
+#else
+#define trace(X...)
+#endif
 
 uint32_t float_to_hex(float number){
 	union {
@@ -12,24 +25,24 @@ uint32_t float_to_hex(float number){
 float fisqrt( float number )
 {	
 	const float x2 = number * 0.5F;
-    printf("Half: %" PRIX32 ".\n", float_to_hex(x2));
-	const float threehalfs = 1.5F;
-    printf("Treehalfs: %" PRIX32 ".\n", float_to_hex(threehalfs));
+    trace("Half: %" PRIX32 ".\n", float_to_hex(x2));
+    const float threehalfs = 1.5F;
+    trace("Treehalfs: %" PRIX32 ".\n", float_to_hex(threehalfs));
 
-	union {
-		float f;
-		uint32_t i;
-	} conv  = { .f = number };
-	conv.i  = 0x5F375A86 - ( conv.i >> 1 );
-    printf("Shifted masked: %" PRIX32 ".\n", float_to_hex(conv.f));
+    union {
+        float f;
+        uint32_t i;
+    } conv  = { .f = number };
+    conv.i  = 0x5F375A86 - ( conv.i >> 1 );
+    trace("Shifted masked: %" PRIX32 ".\n", float_to_hex(conv.f));
     float square = conv.f * conv.f;
-    printf("Square: %" PRIX32 ".\n", float_to_hex(square));
+    trace("Square: %" PRIX32 ".\n", float_to_hex(square));
     float product = x2 * square;
-    printf("Product: %" PRIX32 ".\n", float_to_hex(product));
+    trace("Product: %" PRIX32 ".\n", float_to_hex(product));
     float sub = threehalfs - product;
-    printf("Sub: %" PRIX32 ".\n", float_to_hex(sub));
+    trace("Sub: %" PRIX32 ".\n", float_to_hex(sub));
     float ret = conv.f * sub;
-    printf("Ret: %" PRIX32 ".\n\n", float_to_hex(ret));
+    trace("Ret: %" PRIX32 ".\n\n", float_to_hex(ret));
     return ret;
 }
 
@@ -39,11 +52,12 @@ void fisqrt_tb(int32_t in){
     float flt_out = fisqrt(flt_tmp);
     uint32_t out = (int32_t) flt_out;
     printf("In: %" PRId32 "; out: %" PRId32 "; flt_in: %" PRIX32 "; flt_tmp %" PRIX32 "; flt_out %" PRIX32 ".\n", in, out, float_to_hex(flt_in), float_to_hex(flt_tmp), float_to_hex(flt_out));
+    trace("\n---------------\n\n");
 }
 
 int main(void){
     fisqrt_tb(654);
-    return 0;
+    /*return 0;*/
     fisqrt_tb(897);
     fisqrt_tb(7654);
     fisqrt_tb(1245);
