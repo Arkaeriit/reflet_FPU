0dee86857183ff74cb30bf1698d4d2d7830b5e51
Arkaeriit <maxbouillot@gmail.com>
2021-07-19 19:42:33 +0200

    Cleaned the API a bit.

--- tools/half_float.c
+++ tools/half_float.c
@@ -1,5 +1,24 @@
 #include "half_float.h"
 
+/*---------------------------------\
+|Manipulating 16 bits float numbers|
+|as a struct for opperations.      |
+\---------------------------------*/
+
+#include <stdbool.h>
+
+typedef struct {
+    bool sign;         //True if negative
+    int8_t exponent;   //Biased exponent
+    uint16_t mantissa; //Mantissa, including the starting 1
+} half_float_s;
+
+//Functions
+static void half_float_comply(half_float_s* hf);
+static void float_to_half_s(float f, half_float_s* hf);
+static float half_s_to_float(const half_float_s* hf);
+
+
 #define EXPONENT_SIZE      5
 #define MANTISSA_SIZE      10// Note: this size does not take into account the first 1 in the struct
 #define EXPONENT_BIAIS     15
@@ -13,7 +32,7 @@
 #define F_32_EXPONENT_MASK  0xFF
 
 //use mask to constrain the value of a half_float
-void half_float_comply(half_float_t* hf){
+static void half_float_comply(half_float_s* hf){
     //Check exponent value
     int8_t unbiased_exponent = hf->exponent - EXPONENT_BIAIS;
     if(unbiased_exponent < -14){ //Set number to 0
@@ -42,7 +61,7 @@ void half_float_comply(half_float_t* hf){
 }
 
 //Convert a float into an half float
-void float_to_half(float f, half_float_t* hf){
+static void float_to_half_s(float f, half_float_s* hf){
     union {
         uint32_t i;
         float f;
@@ -61,7 +80,7 @@ void float_to_half(float f, half_float_t* hf){
 }
 
 //Convert a half_float into a float
-float half_to_float(const half_float_t* hf){
+static float half_s_to_float(const half_float_s* hf){
     int8_t unbiased_exponent = hf->exponent - EXPONENT_BIAIS;
     int16_t full_mantissa = hf->mantissa;
     float ret = full_mantissa;
@@ -79,11 +98,65 @@ float half_to_float(const half_float_t* hf){
     return ret;
 }
 
+/*--------------------------\
+|Converting 16 bits number  |
+|from the convinient struct.|
+\--------------------------*/
+
+//Convert the struct into 16 bits
+static half_float_t struct_to_num(half_float_s* s){
+    half_float_t ret = 0;
+    ret |= s->mantissa & CUT_MANTISSA_MASK;
+    ret |= (s->exponent & EXPONENT_MASK) << MANTISSA_SIZE;
+    if(s->sign){
+        ret |= 1 << 15;
+    }
+    return ret;
+}
+
+//Convert 16 bit number into the struct
+static void num_to_struct(half_float_t num, half_float_s* s){
+    if(num & (1 << 15)){
+        s->sign = true;
+    }else{
+        s->sign = false;
+    }
+    if(!(num & (CUT_MANTISSA_MASK | (EXPONENT_MASK << MANTISSA_SIZE)))){
+        s->exponent = 0;
+        s->mantissa = 0;
+        return;
+    }
+    s->mantissa = (num & CUT_MANTISSA_MASK) | (1 << MANTISSA_SIZE);
+    s->exponent = (num >> MANTISSA_SIZE) & EXPONENT_MASK;
+}
+
+/*----------------------\
+|Public API manipulating|
+|the 16 bit numbers.    |
+\----------------------*/
+
+//Convert 32 bits floats into 16 bits ones
+half_float_t float_to_half(float f){
+    half_float_s hf;
+    float_to_half_s(f, &hf);
+    return struct_to_num(&hf);
+}
+
+//Convert 16 bits floats into 32 bits ones
+float half_to_float(half_float_t f){
+    half_float_s hf;
+    num_to_struct(f, &hf);
+    return half_s_to_float(&hf);
+}
+
+//
+// Some testing
+//
+
 #include "stdio.h"
 void test(float f){
-    half_float_t hf;
-    float_to_half(f, &hf);
-    float conv = half_to_float(&hf);
+    half_float_t hf = float_to_half(f);
+    float conv = half_to_float(hf);
     printf("f: %f, conv: %f.\n", f, conv);
 }
 
--- tools/half_float.h
+++ tools/half_float.h
@@ -2,20 +2,12 @@
 #define HALF_FLOAT_H
 
 #include <stdint.h>
-#include <stdbool.h>
 
-typedef struct {
-    bool sign;         //True if negative
-    int8_t exponent;  //Biased exponent
-    uint16_t mantissa; //Mantissa, including the starting 1
-} half_float_t;
-
-//Constrict the value of a half float by masking its fields
-void half_float_comply(half_float_t* hf);
+typedef uint16_t half_float_t;
 
 //Conversions
-void float_to_half(float f, half_float_t* hf);
-float half_to_float(const half_float_t* hf);
+half_float_t float_to_half(float f);
+float half_to_float(half_float_t hf);
 
 #endif
 
