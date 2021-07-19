25b81536219986b023835fbb88e328c337948613
Arkaeriit <maxbouillot@gmail.com>
2021-07-20 00:26:33 +0200

    Paralellize the 16 bit magic word tool.

--- tools/finding_16b_magic_word.c
+++ tools/finding_16b_magic_word.c
@@ -1,6 +1,7 @@
 #include "half_float.h"
 #include <math.h>
 #include <stdio.h>
+#include <threads.h>
 
 //Compute the fast inverse square root of a 16 bit floating point number, given a specific magic word
 half_float_t fisqrt16(half_float_t number, uint16_t magic_number){	
@@ -30,14 +31,15 @@ double average_error(uint16_t magic_number){
     return ret / (0x7FFF - 0x400);
 }
 
-//This function compute the magic number with the minimal average  error for the fisqrt
-uint16_t best_magic_number(void){
-    uint16_t magic = 0;
-    uint16_t best_magic = 0;
+//This function compute the magic number with the minimal average  error for the fisqrt in the given interval
+//The minimal error can be aquiered from the pointer in the argument
+uint16_t best_magic_number_range(uint16_t start, uint16_t stop, double* min_error_ret){
+    uint16_t magic = start;
+    uint16_t best_magic = start;
     double min_error = average_error(magic);
     magic++;
-    while(magic){
-        printf("%04X\n", magic);
+    while(magic && magic <= stop){
+        /*printf("%04X\n", magic);*/
         double error = average_error(magic);
         if(error < min_error){
             min_error = error;
@@ -45,11 +47,60 @@ uint16_t best_magic_number(void){
         }
         magic++;
     }
+    *min_error_ret = min_error;
+    return best_magic;
+}
+
+//This is a wraper over best_magic_number_range threadable
+struct best_magic_thread_s {
+    volatile double ret_err;
+    volatile uint16_t ret_number;
+    uint16_t start;
+    uint16_t stop;
+};
+int best_magic_thread(void* arg_void){
+    struct best_magic_thread_s* arg = arg_void;
+    double ret_err;
+    arg->ret_number = best_magic_number_range(arg->start, arg->stop, &ret_err);
+    arg->ret_err = ret_err;
+    return 0;
+}
+
+//This function runs a bunch of threads to find the best magic number
+uint16_t best_magic_number(int thread_count){
+    //Launchng threads
+    struct best_magic_thread_s arg_list[thread_count];
+    thrd_t thread_list[thread_count];
+    for(int i=0; i<thread_count-1; i++){
+        printf("Launched thread %i/%i.\n", i+1, thread_count);
+        arg_list[i].start = ((uint64_t) 0xFFFF / (uint64_t) thread_count) * i;
+        arg_list[i].stop = ((uint64_t) 0xFFFF / (uint64_t) thread_count) * (i+1) - 1;
+        thrd_create(&thread_list[i], best_magic_thread, &arg_list[i]); 
+    }
+    arg_list[thread_count-1].start = ((uint64_t) 0xFFFF / (uint64_t) thread_count) * (thread_count - 1);
+    arg_list[thread_count-1].stop = 0xFFFF;
+    thrd_create(&thread_list[thread_count-1], best_magic_thread, &arg_list[thread_count-1]); 
+    printf("Launched thread %i/%i.\n", thread_count, thread_count);
+    //Getting the result
+    thrd_join(thread_list[1], NULL);
+    printf("Joined thread %i/%i.\n", 1, thread_count);
+    uint16_t best_magic = arg_list[1].ret_number;
+    double min_error = arg_list[1].ret_err;
+    for(int i=1; i<thread_count; i++){
+        thrd_join(thread_list[i], NULL);
+        printf("Joined thread %i/%i.\n", i+1, thread_count);
+        uint16_t magic = arg_list[i].ret_number;
+        double error = arg_list[i].ret_err;
+        if(error < min_error){
+            best_magic = magic;
+            min_error = error;
+        }
+    }
     return best_magic;
 }
 
 int main(void){
-    printf("The best magic number for the 16 bits fisqrt is %X.\n", best_magic_number());
+    printf("The best magic number for the 16 bits fisqrt is %X.\n", best_magic_number(4));
     return 0;
 }
 
