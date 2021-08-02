7afac588df1134cba2c99830d3bf1d7df1d5356d
Arkaeriit <maxbouillot@gmail.com>
2021-08-02 20:47:36 +0200

    Improved and tested AU.
    It somewhat works but the sign
    is broken on inverse and div and
    trimult, cube and multadd are broken.

--- float_processing/reflet_float_au.v
+++ float_processing/reflet_float_au.v
@@ -30,65 +30,68 @@ module reflet_float_au #(
         .enable(enable & fisqrt_en),
         .in(fisqrt_in),
         .out(fisqrt_out),
-        .ready(fisqrt_out));
+        .ready(fisqrt_rdy));
 
     //Multiplication modules
-    wire [float_size-1:0] mult_1_in1, mult_1_in2, mult_2_in1, mult_2_in2, mult_1_out, mult_2_out;
+    wire [float_size-1:0] mult_1_flt_in1, mult_1_flt_in2, mult_2_flt_in1, mult_2_flt_in2, mult_1_out, mult_2_out;
     wire mult_1_en, mult_2_en, mult_1_rdy, mult_2_rdy;
     reflet_float_mult #(float_size) mult_1 (
         .clk(clk),
         .enable(enable & mult_1_en),
         .ready(mult_1_rdy),
-        .in1(mult_1_in1),
-        .in2(mult_1_in2),
-        .out(mult_1_out));
+        .in1(mult_1_flt_in1),
+        .in2(mult_1_flt_in2),
+        .mult(mult_1_out));
     reflet_float_mult #(float_size) mult_2 (
         .clk(clk),
         .enable(enable & mult_2_en),
         .ready(mult_2_rdy),
-        .in1(mult_2_in1),
-        .in2(mult_2_in2),
-        .out(mult_2_out));
+        .in1(mult_2_flt_in1),
+        .in2(mult_2_flt_in2),
+        .mult(mult_2_out));
 
     //Addition module
-    wire [float_size-1:0] add_in1, add_in2, add_out;
+    wire [float_size-1:0] add_flt_in1, add_flt_in2, add_out;
     wire add_en, sub_en;
     reflet_float_add #(float_size) add (
-        .in1(add_in1),
-        .in2(add_in2),
+        .in1(add_flt_in1),
+        .in2(add_flt_in2),
         .sum(add_out),
         .enable_add(enable & add_en),
         .enable_sub(enable & sub_en));
 
     //Interconect
     //fisqrt inputs
-    assign fisqrt_en = opcode == OPP_FISQRT || opcode == OPP_DIV || opcode == OPP_INV;
-    assign fisqrt_in = ( opcode == OPP_DIV ? in2 : in1 ); //In most case we take in1 as input inless we are dividing. In the other case the module is not enabled
+    assign fisqrt_en = opcode == `OPP_FISQRT || opcode == `OPP_DIV || opcode == `OPP_INV;
+    assign fisqrt_in = ( opcode == `OPP_DIV ? flt_in2 : flt_in1 ); //In most case we take flt_in1 as input inless we are dividing. In the other case the module is not enabled
 
     //add inputs
-    assign add_en = opcode == OPP_ADD || (opcode == OPP_MULTADD && mult_1_rdy);
-    assign sub_en = opcode == OPP_SUB;
-    assign add_in1 = ( opcode == OPP_MULTADD ? mult_2_out : in1 );
-    assign add_in2 = ( opcode == OPP_MULTADD ? in3 : in2 );
+    assign add_en = opcode == `OPP_ADD || (opcode == `OPP_MULTADD && mult_1_rdy);
+    assign sub_en = opcode == `OPP_SUB;
+    assign add_flt_in1 = ( opcode == `OPP_MULTADD ? mult_2_out : flt_in1 );
+    assign add_flt_in2 = ( opcode == `OPP_MULTADD ? flt_in3 : flt_in2 );
 
     //mult1 input
-    assign mult_1_en = opcode == OPP_MUL || opcode == OPP_CUBE || opcode == OPP_TESSERACT opcode == OPP_TRIMULT || ((opcode == OPP_DIV || opcode == OPP_INV) && fisqrt_rdy);
-    assign mult_1_in1 = ( opcode == OPP_MUL || opcode == OPP_CUBE || opcode == OPP_TESSERACT || opcode == OPP_TRIMULT ? in1 : fisqrt_out);
-    assign mult_1_in2 = ( opcode == OPP_MUL || opcode == OPP_TRIMULT ? in2 : ( opcode == OPP_CUBE || opcode == OPP_TESSERACT ? in1 : fisqrt_out));
+    assign mult_1_en = opcode == `OPP_MUL || opcode == `OPP_CUBE || opcode == `OPP_TESSERACT || opcode == `OPP_TRIMULT || ((opcode == `OPP_DIV || opcode == `OPP_INV) && fisqrt_rdy);
+    assign mult_1_flt_in1 = ( (opcode == `OPP_MUL || opcode == `OPP_CUBE || opcode == `OPP_TESSERACT || opcode == `OPP_TRIMULT) ? flt_in1 : fisqrt_out);
+    assign mult_1_flt_in2 = ( (opcode == `OPP_MUL || opcode == `OPP_TRIMULT) ? flt_in2 :
+                                ( opcode == `OPP_CUBE || opcode == `OPP_TESSERACT ? flt_in1 : fisqrt_out));
 
     //mult2 input
-    assign mult_2_en = ( opcode == OPP_DIV || opcode == OPP_CUBE || opcode == OPP_TESSERACT || opcode == OPP_TRIMULT ? mult_1_rdy : opcode == OPP_MULTADD )
-    assign mult_2_in1 = ( opcode == OPP_DIV || opcode == OPP_CUBE || opcode == OPP_TESSERACT || opcode == OPP_TESSERACT ? mult_1_out : in1 );
-    assign mult_2_in2 = ( opcode == OPP_CUBE || opcode == OPP_MULTADD || opcode == OPP_DIV ? in2 : ( opcode == OPP_TRIMULT ? in3 : mult_1_out ) );
+    assign mult_2_en = opcode == `OPP_DIV || opcode == `OPP_CUBE || opcode == `OPP_TESSERACT || ( opcode == `OPP_TRIMULT ? mult_1_rdy : opcode == `OPP_MULTADD );
+    assign mult_2_flt_in1 = ( opcode == `OPP_DIV || opcode == `OPP_CUBE || opcode == `OPP_TESSERACT || opcode == `OPP_TESSERACT ? mult_1_out : flt_in1 );
+    assign mult_2_flt_in2 = ( (opcode == `OPP_CUBE || opcode == `OPP_MULTADD) ? flt_in2 : 
+                              ( opcode == `OPP_TRIMULT ? flt_in3 :
+                                ( opcode == `OPP_DIV ? flt_in1 : mult_1_out )));
 
     //Global outputs
-    assign out = ( opcode == OPP_MULTADD || opcode == OPP_ADD || opcode == OPP_SUB ? add_out :
-                   ( opcode == OPP_DIV || opcode == OPP_CUBE || opcode == OPP_TESSERACT || opcode == OPP_TRIMULT ? mult_2_out :
-                     ( opcode == OPP_MUL || opcode == OPP_INV ? mult_1_out : fisqrt_out )));
-    assign ready = ( opcode == OPP_ADD || opcode == OPP_SUB ? enable :
-                   ( opcode == OPP_DIV || opcode == OPP_CUBE || opcode == OPP_TESSERACT || opcode == OPP_TRIMULT || opcode == OPP_MULTADD ? mult_2_rdy :
-                     ( opcode == OPP_MUL || opcode == OPP_INV ? mult_1_rdy : 
-                       ( opcode == OPP_FISQRT ? fisqrt_out : 0 ))));
+    assign flt_out = ( opcode == `OPP_MULTADD || opcode == `OPP_ADD || opcode == `OPP_SUB ? add_out :
+                   ( opcode == `OPP_DIV || opcode == `OPP_CUBE || opcode == `OPP_TESSERACT || opcode == `OPP_TRIMULT ? mult_2_out :
+                     ( opcode == `OPP_MUL || opcode == `OPP_INV ? mult_1_out : fisqrt_out )));
+    assign ready = ( opcode == `OPP_ADD || opcode == `OPP_SUB ? enable :
+                   ( opcode == `OPP_DIV || opcode == `OPP_CUBE || opcode == `OPP_TESSERACT || opcode == `OPP_TRIMULT || opcode == `OPP_MULTADD ? mult_2_rdy :
+                     ( opcode == `OPP_MUL || opcode == `OPP_INV ? mult_1_rdy : 
+                       ( opcode == `OPP_FISQRT ? fisqrt_out : 0 ))));
 
 endmodule
 
---
+++ simu/reflet_float_au_tb.v
@@ -0,0 +1,72 @@
+
+module reflet_float_au_tb();
+
+    reg clk = 1;
+    always #1 clk <= !clk;
+
+    reg [5:0] opcode = 0;
+    wire ready;
+
+    reg signed [15:0] in1 = 56;
+    reg signed [15:0] in2 = -549;
+    reg signed [15:0] in3 = 324;
+    
+    wire [31:0] flt1, flt2, flt3, flt_out;
+    wire signed [31:0] out_int;
+
+    //Conversion
+    reflet_int_to_float #(.int_size(16), .float_size(32)) itf1 (.int_in(in1), .float_out(flt1));
+    reflet_int_to_float #(.int_size(16), .float_size(32)) itf2 (.int_in(in2), .float_out(flt2));
+    reflet_int_to_float #(.int_size(16), .float_size(32)) itf3 (.int_in(in3), .float_out(flt3));
+    reflet_float_to_int #(.int_size(32), .float_size(32)) fti (.float_in(flt_out), .int_out(out_int));
+
+    //Arithmetic unit
+    reflet_float_au #(.float_size(32)) au (
+        .clk(clk),
+        .enable(1'b1),
+        .opcode(opcode),
+        .ready(ready),
+        .flt_in1(flt1),
+        .flt_in2(flt2),
+        .flt_in3(flt3),
+        .flt_out(flt_out),
+        .flag_out());
+
+    initial
+    begin
+        $dumpfile("reflet_float_au_tb.vcd");
+        $dumpvars(0, reflet_float_au_tb);
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        #100;
+        opcode <= opcode + 1;
+        $finish;
+    end
+
+
+endmodule
+
