9a343e9a3628c47ab0685f817228ce06fe35cc51
Arkaeriit <maxbouillot@gmail.com>
2021-09-30 21:57:39 +0200

    Fixed bug with alignment directives.
    The alignment directives would not take
    into account the size of addresses.

--- assembler/code.asm
+++ assembler/code.asm
@@ -2,3 +2,4 @@ nop
 add R3 R4 R1
 mul R3 R3 R3
 sub R3 R4 R5
+fisqrt R7 R6
--- assembler/directives.asm
+++ assembler/directives.asm
@@ -1,5 +1,7 @@
 @import code.asm
 @align 16
+@labref coucou
+@align 16
 @label coucou
 @rawbytes 12 34 56 78 9A BC DE F0
 @labref coucou
