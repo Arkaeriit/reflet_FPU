bc643523b4f59c2323ed82d54514236ee3b674b4
Arkaeriit <maxbouillot@gmail.com>
2021-09-30 22:52:54 +0200

    Improve addr handeling and block manipulation.
    * Runtime check are made in block
    manipulations and the manipulations
    are made from simple operations.
    * Addr offset count works and do not
    use dummy blocks anymore.

--- assembler/directives.asm
+++ assembler/directives.asm
@@ -1,8 +1,12 @@
+@label start
 @import code.asm
+@label endCode
 @align 16
 @labref coucou
 @align 16
 @label coucou
 @rawbytes 12 34 56 78 9A BC DE F0
 @labref coucou
+@labref start
+@labref endCode
 
